2534606686a0a128b1fa4d38583e2e00b2ec7747
Navojith <navojith22@gmail.com>
2025-05-26 00:12:45 +0530

    animate

--- src/pages/domain/Domain.tsx
+++ src/pages/domain/Domain.tsx
@@ -15,52 +15,111 @@ import Objectives from './components/Objectives';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import SystemDiagram from '../../assets/System-Diagram.png';
 import Technologies from './components/Technologies';
+import { motion } from 'framer-motion';
+
+const fadeInUp = {
+  hidden: { opacity: 0, y: 30 },
+  visible: { opacity: 1, y: 0 },
+};
 
 const Domain = () => {
   const literature = data['background-literature'];
   const methodology = data['methodology'];
+
   return (
-    <Container sx={{ mt: 6 }}>
-      <Stack spacing={5}>
-        <TableView
-          title={literature.title}
-          desc={literature.desc}
-          image={
-            <img src={LiteratureImage} alt="literature image" width={1500} />
-          }
-          references={literature.ref}
-        />
-        <ResearchGap />
-        <Problem />
-        <Objectives />
-        <TableView
-          title={methodology.title}
-          desc={methodology.desc}
-          image={
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1-content"
-                id="panel1-header"
-                sx={{ justifyContent: 'center' }}
-              >
-                <Typography variant="h5">System Diagram</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
+    <Stack pb={6}>
+      <Container sx={{ mt: 6 }}>
+        <Stack spacing={5}>
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={fadeInUp}
+            transition={{ duration: 0.6 }}
+          >
+            <TableView
+              title={literature.title}
+              desc={literature.desc}
+              image={
                 <img
-                  src={SystemDiagram}
-                  alt="SystemDiagram image"
+                  src={LiteratureImage}
+                  alt="literature image"
                   width={1500}
                 />
-              </AccordionDetails>
-            </Accordion>
-          }
-          imgInAccordion
-        />
-        <Technologies />
-      </Stack>
-    </Container>
+              }
+              references={literature.ref}
+            />
+          </motion.div>
+
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={fadeInUp}
+            transition={{ duration: 0.6, delay: 0.1 }}
+          >
+            <ResearchGap />
+          </motion.div>
+
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={fadeInUp}
+            transition={{ duration: 0.6, delay: 0.2 }}
+          >
+            <Problem />
+          </motion.div>
+
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={fadeInUp}
+            transition={{ duration: 0.6, delay: 0.3 }}
+          >
+            <Objectives />
+          </motion.div>
+
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={fadeInUp}
+            transition={{ duration: 0.6, delay: 0.4 }}
+          >
+            <TableView
+              title={methodology.title}
+              desc={methodology.desc}
+              image={
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1-content"
+                    id="panel1-header"
+                    sx={{ justifyContent: 'center' }}
+                  >
+                    <Typography variant="h5">System Diagram</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <img
+                      src={SystemDiagram}
+                      alt="SystemDiagram image"
+                      width={1500}
+                    />
+                  </AccordionDetails>
+                </Accordion>
+              }
+              imgInAccordion
+            />
+          </motion.div>
+
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={fadeInUp}
+            transition={{ duration: 0.6, delay: 0.5 }}
+          >
+            <Technologies />
+          </motion.div>
+        </Stack>
+      </Container>
+    </Stack>
   );
 };
-
 export default Domain;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,14 @@
-import { Container } from '@mui/material';
+import { Container, Stack } from '@mui/material';
 import HomeComponent from '../../components/Home/HomeComponent';
 import '../../css/home.css';
 
 const Home = () => {
   return (
-    <Container className="homeContainer" sx={{ mt: 6 }}>
-      <HomeComponent />
-    </Container>
+    <Stack pb={6}>
+      <Container className="homeContainer" sx={{ mt: 6 }}>
+        <HomeComponent />
+      </Container>
+    </Stack>
   );
 };
 
